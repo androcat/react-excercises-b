12a40cfa4a2a0296b0ed983ada263ad76927b5a1
Jay Medina <jay.medina.celis@gmail.com>
2023-02-01 19:38:00 -0500

    Adds person info to state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import InfoForm from "./components/InfoForm";
+import { useState } from "react";
 
 function App() {
-  return <InfoForm />;
+  const [persons, setPerson] = useState([]);
+
+  const addPerson = (person) => {
+    setPerson([...persons, person]);
+  };
+
+  return <InfoForm addPerson={addPerson} />;
 }
 
 export default App;
--- src/components/InfoForm.js
+++ src/components/InfoForm.js
@@ -1,17 +1,57 @@
-function InfoForm() {
+import { useState } from "react";
+
+function InfoForm({ addPerson }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newPerson = {
+      firstName,
+      lastName,
+      address,
+      phoneNumber,
+    };
+
+    addPerson(newPerson);
+    setFirstName("");
+    setLastName("");
+    setAddress("");
+    setPhoneNumber("");
+  };
+
   return (
-    <form>
+    <form className="personForm" onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="firstName" name="firstName" className="form-label">
           First Name
         </label>
-        <input type="text" className="form-control" placeholder="Alex" />
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+          className="form-control"
+          placeholder="Alex"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="lastName" name="lastName" className="form-label">
           Last Name
         </label>
-        <input type="text" className="form-control" placeholder="Smith" />
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+          className="form-control"
+          placeholder="Smith"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="address" name="address" className="form-label">
@@ -19,6 +59,10 @@ function InfoForm() {
         </label>
         <input
           type="text"
+          value={address}
+          onChange={(e) => {
+            setAddress(e.target.value);
+          }}
           className="form-control"
           placeholder="123 Lemon Lane"
         />
@@ -27,7 +71,15 @@ function InfoForm() {
         <label htmlFor="phoneNumber" name="phoneNumber" className="form-label">
           Phone Number
         </label>
-        <input type="tel" className="form-control" placeholder="Alex" />
+        <input
+          type="tel"
+          value={phoneNumber}
+          onChange={(e) => {
+            setPhoneNumber(e.target.value);
+          }}
+          className="form-control"
+          placeholder="Alex"
+        />
       </div>
       <button type="submit">Save</button>
     </form>
